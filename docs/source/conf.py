32a960b817f0c900e7723be84bc6a1a205425c5b
HenrryShen <henrry@rocketmail.com>
2023-10-01 00:09:23 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Wikisites'
+copyright = '2023, Wikisites'
+author = 'Henrry'
 
 release = '0.1'
 version = '0.1.0'
